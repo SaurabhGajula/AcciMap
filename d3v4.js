229cd6ea87fc054f2dde456f38446d84f37586da
SaurabhGajula <saurabh.gajula@gmail.com>
2017-03-17 21:18:43 +0530

    New add+2 on friday

--- d3v4.js
+++ d3v4.js
@@ -1,4 +1,4 @@
-// https://d3js.org Version 4.4.0. Copyright 2016 Mike Bostock.
+// https://d3js.org Version 4.4.0. 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
